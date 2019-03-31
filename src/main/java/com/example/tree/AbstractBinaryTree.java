a98329bbfe8d6c4122cc5011ed6102184815b894
Henry Dang <henry@baffle.io>
2019-03-30 21:17:55 -0700

    Woring on tree...

---
+++ src/main/java/com/example/tree/AbstractBinaryTree.java
@@ -0,0 +1,43 @@
+package com.example.tree;
+
+import java.util.List;
+import java.util.ArrayList;
+import com.example.list.Position;
+
+public class AbstractBinaryTree<E> extends AbstractTree<E> implements BinaryTree<E> {
+
+    public Position<E> sibling(Position<E> p) {
+        Position<E> parent = parent(p);
+        if (parent == null) {
+            return null;
+        }
+        if (p == left(parent)) {
+            return right(parent);
+        } else {
+            return left(parent);
+        }
+    }
+
+    public int numChildren(Position<E> p) {
+        int count = 0;
+        if (left(p) != null) {
+            count++;
+        }
+        if (right(p) != null) {
+            count++;
+        }
+        return count;
+    }
+
+    public Iterable<Position<E>> children(Position<E> p) {
+        List<Position<E>> snapshot = new ArrayList<>(2);
+        if (left(p) != null) {
+           snapshot.add(left(p));
+        }
+        if (right(p) != null) {
+            snapshot.add(right(p));
+        }
+        return snapshot;
+    }
+}
+
